e536e6bb5d7435e90fdef9540ac630d44d53d8e9
Jabraan Altaf <46460148+JabraanAltaf@users.noreply.github.com>
2019-08-26 23:11:52 +0100

    Add files via upload

---
+++ Web Scraping IMDB Top 50.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[161]:
+
+
+from bs4 import BeautifulSoup
+import requests
+
+# webpage to be scraped
+url = "https://www.imdb.com/search/title/?groups=top_250&sort=user_rating"
+response = requests.get(url)
+
+# Beautiful Soup takes params -> response as text and parser type
+soup = BeautifulSoup(response.text)
+
+# Selecting the HTML values needed
+movies = soup.select('.lister-item-header')
+ratings = soup.select('.ratings-bar strong')
+
+# variable for index of ratings for a movie
+index = 0
+
+# Printing out the top 50 movies including it's IMDB rating
+for movie in movies:
+     print(movie.select_one('.lister-item-index').getText() + movie.find('a').getText())
+     print("Rating: " + ratings[index].getText())
+     index += 1
+
+
+# In[ ]:
+
+
+
+
