5fe0dc2645e504d38767895d3dad0496be536438
Jabraan Altaf <46460148+JabraanAltaf@users.noreply.github.com>
2019-08-26 20:09:51 +0100

    Update API.py

--- API.py
+++ API.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[49]:
-
-
 import requests
-
-
-# In[50]:
-
 
 url = "https://api.yelp.com/v3/businesses/search"
     
-api_key = "tTmPcJxm5WwrPcgcX9jX6QIr140bLkxsSsEWEQZR-m5qknEISxNGa7I_20DPnOAZtl0cWh1qlWGNdUmr3_L5D72gFoH70xkD530Do9PAcTNpdzTGxnFBjsvPJwxkXXYx"
+api_key = API_KEY
 
 # Key and Value of main header request
 headers = {
@@ -27,22 +17,14 @@ params = {
 
 # Response takes in a header and params variable
 response = requests.get(url, headers = headers, params = params)
-#print(response.text)
 #print(response.json())
 
 # Getting a json of all the food businesses 
 foodPlaces = response.json()["businesses"]
-#print(business)
 
 # Printing out all the names of the food placess
 for place in foodPlaces:
     print(place["name"])
-
-
-
-
-# In[51]:
-
 
 # Find places rated 3.5 or better and store them in a list
 wellRated = [place["name"] + " " + str(place["rating"]) for place in foodPlaces if place["rating"] > 3.5]
@@ -50,16 +32,3 @@ wellRated = [place["name"] + " " + str(place["rating"]) for place in foodPlaces 
 
 # Print the name and rating of the first place in the list
 print(wellRated[0])
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
